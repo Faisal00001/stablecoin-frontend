61026f8b4c7d7fbfca0d14e11dd08aab6c517b17
Faisal00001 <faisalosman798@gmail.com>
2025-01-18 13:54:56 +0600

    Added more option

--- src/Dashboard/Overview/Overview.jsx
+++ src/Dashboard/Overview/Overview.jsx
@@ -4,27 +4,110 @@ import PortfolioCard from "../../components/PortfolioCard/PortfolioCard";
 import MyAssetCard from "../../components/MyAssetCard/MyAssetCard";
 import { CiWallet } from "react-icons/ci";
 import SalesOverViewChart from "../../components/Overview/SalesOverViewChart";
-
-
-
+import { IoIosInformationCircleOutline } from "react-icons/io";
+import BusinessChart from "../../components/Overview/BusinessChart";
+import { FaPlus } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa6";
+import { IoIosMore } from "react-icons/io";
 const Overview = () => {
     return (
         <div className="flex-1 overflow-auto relative">
             {/* <Header title={'Dashboard'}></Header> */}
-            <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
-                <motion.div
+            <main className="py-6 px-4 lg:px-8">
+                {/* <motion.div
                     className='grid grid-cols-1 gap-5 md:grid-cols-2 mb-8'
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 1 }}
                 >
                     <PortfolioCard name="My Portfolio" value="$23569.00" icon={CiWallet} />
-                    {/* <MyAssetCard name="My Asset" value="$1523.00" value_2="$2145.00" /> */}
-                    {/* <PortfolioCard name="My Portfolio" value="$23569.00" icon={CiWallet}></PortfolioCard> */}
-                </motion.div>
+                    
+                </motion.div> */}
+                <h3 className="text-2xl font-bold">All business accounts</h3>
+                <div className="mt-10 flex gap-10">
+                    <div className="flex gap-20 flex-[0.8]">
+                        <div className="space-y-1 w-[30%]">
+                            <h3 className="font-bold text-lg">Total balance</h3>
+                            <p className="text-3xl font-bold">$5,999,567.25</p>
+                            <div className="flex gap-2 items-center">
+                                <p>
+                                    $112,241,21.98
+                                </p>
+                                <p>pending</p>
+                                <span>
+                                    <IoIosInformationCircleOutline className="text-xl" />
+                                </span>
+                            </div>
+                        </div>
+                        <div className="w-[70%]">
+                            <BusinessChart />
+                        </div>
+                    </div>
+                    <div className="flex-[0.2]">
+                        <div className="flex justify-between w-80 p-4 rounded-lg bg-[#FFF7FC] shadow">
+                            <div className="space-y-2">
+                                <h1 className="text-lg font-bold">Send money</h1>
+                                <p className="text-sm text-gray-500">Via ACH, wire, or check</p>
+                            </div>
+                            <div className="flex items-center justify-center w-7 h-7 bg-orange-500 rounded-full">
+                                <span>
+                                    <FaArrowRight className="text-lg text-white" />
+                                </span>
+                            </div>
+                        </div>
+                        <div className="flex justify-between w-80 p-4 rounded-lg bg-[#FFF7FC] shadow mt-3">
+                            <div className="space-y-2">
+                                <h1 className="text-lg font-bold">Transfer money</h1>
+                                <p className="text-sm text-gray-500">Between your accounts</p>
+                            </div>
+                            <div className="flex items-center justify-center w-7 h-7 bg-orange-500 rounded-full">
+                                <span>
+                                    <FaArrowRight className="text-lg text-white" />
+                                </span>
+                            </div>
+                        </div>
+                        <div className="flex justify-between w-80 p-4 rounded-lg bg-[#FFF7FC] shadow mt-3">
+                            <div className="space-y-2">
+                                <h1 className="text-lg font-bold">Add funds</h1>
+                                <p className="text-sm text-gray-500">To your checking account</p>
+                            </div>
+                            <div className="flex items-center justify-center w-7 h-7 bg-orange-500 rounded-full">
+                                <span>
+                                    <FaPlus className="text-lg text-white" />
+                                </span>
+                            </div>
+                        </div>
+                        <div className="flex justify-between w-80 p-4 rounded-lg bg-[#FFF7FC] shadow mt-3">
+                            <div className="space-y-2">
+                                <h1 className="text-lg font-bold">More actions</h1>
+
+                            </div>
+                            <div className="flex items-center justify-center w-7 h-7 bg-orange-500 rounded-full">
+                                <span>
+                                    <IoIosMore className="text-lg text-white" />
+                                </span>
+                            </div>
+                        </div>
+
+                        {/* <div className="card bg-base-100  shadow-xl">
+                            <figure>
+                                <img
+                                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                    alt="Shoes" />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div> */}
+                    </div>
+                </div>
                 {/* Charts */}
                 <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
-                    <SalesOverViewChart />
+                    {/* <SalesOverViewChart /> */}
                 </div>
 
             </main>
---
+++ src/components/Overview/BusinessChart.jsx
@@ -0,0 +1,135 @@
+import { motion } from 'framer-motion';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { useState } from 'react';
+import { FaAngleDown } from 'react-icons/fa6';
+
+const data = [
+    {
+        name: 'Page A',
+        Checking: 4000,
+        Treasury: 2400,
+        Vault: 2400,
+    },
+    {
+        name: 'Page B',
+        Checking: 3000,
+        Treasury: 1398,
+        Vault: 2210,
+    },
+    {
+        name: 'Page C',
+        Checking: 2000,
+        Treasury: 9800,
+        Vault: 2290,
+    },
+    {
+        name: 'Page D',
+        Checking: 2780,
+        Treasury: 3908,
+        Vault: 2000,
+    },
+    {
+        name: 'Page E',
+        Checking: 1890,
+        Treasury: 4800,
+        Vault: 2181,
+    },
+
+];
+const seriesConfig = [
+    { key: 'Checking', color: '#001A6E' },
+    { key: 'Treasury', color: '#80C4E9' },
+    { key: 'Vault', color: '#C4D9FF' }
+];
+const BusinessChart = () => {
+    const [selectedSeries, setSelectedSeries] = useState(null);
+
+    const handleSeriesClick = (dataKey) => {
+        setSelectedSeries(prev => prev === dataKey ? null : dataKey);
+    };
+
+    const CustomLegend = () => (
+        <div className="flex gap-3">
+            {seriesConfig.map(({ key, color }) => (
+                <button
+                    key={key}
+                    onClick={() => handleSeriesClick(key)}
+                    className={`px-3 py-1 rounded ${selectedSeries === null || selectedSeries === key
+                        ? ''
+                        : ' text-gray-400'
+                        }`}
+                >
+                    <div className='flex gap-2 items-center'>
+                        <div className='w-2 h-2 rounded-full' style={{ backgroundColor: color }}>
+                        </div>
+                        <div className='text-sm'>
+                            {key}
+                        </div>
+                    </div>
+                </button>
+            ))}
+        </div>
+    );
+
+    return (
+        <motion.div
+            className='rounded-xl p-6 border border-gray-200 w-full'
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.2 }}
+        >
+            <div className='flex gap-5 items-center mb-3'>
+                <div className="dropdown dropdown-bottom dropdown-end w-32 h-8 rounded border border-gray-200 flex items-center justify-center shadow">
+                    <div tabIndex={0} role="button" className="text-xs flex gap-1 items-center"><span>Last 12 months</span> <span> <FaAngleDown /> </span></div>
+
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded z-[1] w-52 p-2 shadow mt-2">
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <CustomLegend />
+                </div>
+            </div>
+            <div className='h-40'>
+                <ResponsiveContainer width={"100%"} height={"100%"}>
+                    <AreaChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 0,
+                            bottom: 0,
+                        }}
+                    >
+                        {/* <CartesianGrid horizontal={false}
+                            vertical={false}
+                            stroke='#DDDDDD' /> */}
+                        {/* <XAxis dataKey={"name"} />
+                        <YAxis /> */}
+                        <Tooltip
+                            contentStyle={{
+                                backgroundColor: "#FBFBFB",
+
+                            }}
+                            itemStyle={{ color: "#1D1616" }}
+                        />
+                        {(selectedSeries === null || selectedSeries === 'Checking') && (
+                            <Area type="monotone" dataKey="Checking" stackId="1" stroke="#001A6E" fill="#001A6E" fillOpacity={0.9} />
+                        )}
+                        {(selectedSeries === null || selectedSeries === 'Treasury') && (
+                            <Area type="monotone" dataKey="Treasury" stackId="1" stroke="#80C4E9" fill="#80C4E9" fillOpacity={0.9} />
+                        )}
+                        {(selectedSeries === null || selectedSeries === 'Vault') && (
+                            <Area type="monotone" dataKey="Vault" stackId="1" stroke="#C4D9FF" fill="#C4D9FF" fillOpacity={0.9} />
+                        )}
+                    </AreaChart>
+                </ResponsiveContainer>
+            </div>
+        </motion.div>
+    );
+};
+
+export default BusinessChart;
