d434425434b60835538c71be9082dfc0a8c8cdcf
Faisal00001 <faisalosman798@gmail.com>
2025-01-21 00:21:24 +0600

    Remove header in market and wallet pages

--- src/Dashboard/Market/Market.jsx
+++ src/Dashboard/Market/Market.jsx
@@ -1,11 +1,10 @@
 
-import Header from '../../components/common/Header/Header';
 
 const Market = () => {
     return (
 
         <div className="flex-1 overflow-auto relative z-10">
-            <Header title={'Market'}></Header>
+
 
         </div>
     );
